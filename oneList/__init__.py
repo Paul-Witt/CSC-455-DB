333f5a8f15ba8c1211939e488d63df5da7c10477
Paul-Witt <Paulwitt9617@gmail.com>
2020-04-17 00:21:49 -0400

    changed db file check

--- oneList/__init__.py
+++ oneList/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from os import path 
+from os import path, getcwd 
 
 databaseName = "oneList_Database.db"
+
+pwd = getcwd()
 
 
 app = Flask(__name__)
@@ -19,8 +21,11 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+
+print(pwd+"\\oneList\\"+databaseName)
+
 # If there is no database we make one
-if not path.exists("oneList/"+databaseName):
+if not path.exists(pwd+"\\oneList\\"+databaseName):
     from oneList import tools
     tools.makeDB()
 
